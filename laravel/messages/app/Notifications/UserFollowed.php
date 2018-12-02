978f94b72badd9f9a0bb6dbdd30e0a45166929df
Lina Marcela Patiño Pineda <linampatino@gmail.com>
2018-12-02 08:12:13 -0500

    added real time notification fuctionality

--- laravel/messages/app/Notifications/UserFollowed.php
+++ laravel/messages/app/Notifications/UserFollowed.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\BroadcastMessage;
 
 use App\User;
 
@@ -33,7 +34,7 @@ class UserFollowed extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail', 'database'];
+        return ['mail', 'database', 'broadcast'];
     }
 
     /**
@@ -63,5 +64,11 @@ class UserFollowed extends Notification
         return [
             'follower' => $this->follower
         ];
+    }
+
+    public function toBroadcast($notifiable){
+        return new BroadcastMessage([
+            'data' => $this->toArray($notifiable),
+        ]);
     }
 }
--- laravel/messages/resources/assets/js/components/NotificationsComponent.vue
+++ laravel/messages/resources/assets/js/components/NotificationsComponent.vue
@@ -8,6 +8,8 @@
 
 <script>
     export default {
+        props: ['user'],
+
         data(){
             return {
                 notifications: [],
@@ -20,8 +22,14 @@
             axios.get('/api/notifications/')
                 .then( response => {
                     this.notifications = response.data;
+
+                    Echo.private(`App.User.${this.user}`)
+	
+                        .notification(notification => {
+                            this.notifications.unshift(notification);
+                        });
+
                 });
-            console.log('Component mounted. Notifications')
         }
        
         
